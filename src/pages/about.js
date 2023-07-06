f5e2980c8fc0f1b3c0d420d60ad58651ed1fa6d0
Gunnar <gnnrbachelor@gmail.com>
2023-07-06 09:18:31 -0600

    Reformat About

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,17 +14,19 @@ const AboutPage = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <div className="article-card">
         <div className="about-title"><h1>About</h1></div>
-        <StaticImage
-          className="aboutImage"
-          formats={["auto", "webp", "avif"]}
-          src="../images/about.png"
-          width={600}
-          height={600}
-          alt="Psych0p0mp"
-        />
-        <p className="about-us">Psych0p0mp is a digital journal dedicated to psychedelic art and alternative spirituality. I started it with the aim of celebrating the vibrant and mind-bending world of psychedelic art. I aim explore the rich history of the genre, from its origins in the counterculture of the 1960s to its continued evolution today.
+        <div className="about-container">
+          <StaticImage
+            className="aboutImage"
+            formats={["auto", "webp", "avif"]}
+            src="../images/about.png"
+            width={600}
+            height={600}
+            alt="Psych0p0mp"
+          />
+          <p className="about-us">Psych0p0mp is a digital journal dedicated to psychedelic art and alternative spirituality. I started it with the aim of celebrating the vibrant and mind-bending world of psychedelic art. I aim explore the rich history of the genre, from its origins in the counterculture of the 1960s to its continued evolution today.
 
 I believe that art is more than just visual spectacle, it is a means of exploring the deepest reaches of the human psyche, of expanding consciousness and unlocking the mysteries of human experience. For updates, follow me on <a href="https://twitter.com/justgnnr">Twitter</a>.</p>
+        </div>
       </div>
     </Layout>
   )
