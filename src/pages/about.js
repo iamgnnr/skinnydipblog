9a8e7f782082106eee59b80b1439d2bbe10d96d1
Gunnar <gnnrbachelor@gmail.com>
2023-07-10 08:23:29 -0600

    Fix about

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,9 +23,19 @@ const AboutPage = ({ data, location }) => {
             height={600}
             alt="Psych0p0mp"
           />
-          <p className="about-us">Psych0p0mp is a digital journal dedicated to psychedelic art and alternative spirituality. I started it with the aim of celebrating the vibrant and mind-bending world of psychedelic art. I aim explore the rich history of the genre, from its origins in the counterculture of the 1960s to its continued evolution today.
+          <p className="about-us">
 
-I believe that art is more than just visual spectacle, it is a means of exploring the deepest reaches of the human psyche, of expanding consciousness and unlocking the mysteries of human experience. For updates, follow me on <a href="https://twitter.com/justgnnr">Twitter</a>.</p>
+At Skinny Dip, we believe in the power of storytelling and the art of weaving narratives that capture the essence of real-life experiences. Through our blog, we strive to celebrate the beauty, complexity, and authenticity of the human experience by curating a collection of thought-provoking and captivating essays.
+
+Our team of passionate writers, avid readers, and literary enthusiasts are dedicated to exploring the vast landscape of human stories, transcending genres and boundaries. We seek to delve into the depths of personal journeys, memories, and observations, bringing forth the raw emotions and universal truths that connect us all.
+
+Our blog serves as a platform for both established and emerging writers to share their unique perspectives and narratives. We are passionate about fostering a diverse and inclusive community, where voices from different backgrounds, cultures, and experiences can be heard and appreciated. Through these diverse voices, we hope to inspire empathy, understanding, and connection among our readers.
+
+In addition to our curated collection of essays, memoirs, and personal narratives, we also provide valuable resources and writing tips for aspiring creative nonfiction writers. We believe in nurturing and supporting the literary community, offering guidance and encouragement to those who seek to explore the realm of nonfiction storytelling.
+
+Whether you are an avid reader, a writer searching for inspiration, or someone simply curious about the power of personal narratives, we invite you to embark on this journey with us. Together, let's celebrate the magic of true stories and the incredible power of personal testimony.
+
+Welcome to Skinny Dip, where creativity meets truth.</p>
         </div>
       </div>
     </Layout>
