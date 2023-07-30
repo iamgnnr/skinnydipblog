1d94cb22785da5f1a0f70f5d27a9b1a548eb03fe
Gunnar <gnnrbachelor@gmail.com>
2023-07-29 18:41:19 -0600

    Add link to Medium article

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,7 +26,7 @@ const AboutPage = ({ data, location }) => {
           <p className="about-us">
             Welcome to Skinny Dip, a blog dedicated to unveiling the enchanting intersection of technology and nature through the art of poetry. I built this blog is a testament to the harmonious coexistence of two seemingly disparate realms – the ever-evolving virtual landscape and the timeless beauty of Nature. Every poem and image featured here is created by artificial intelligence, and inspired by the natural world.
 
-In a world where screens have become our windows to the outside, this blog is an invitation to embark on a poetic journey that marries the wonders of technology with the awe-inspiring creations of nature. To learn more about how I built this blog check out my Medium article about the project.
+In a world where screens have become our windows to the outside, this blog is an invitation to embark on a poetic journey that marries the wonders of technology with the awe-inspiring creations of nature. To learn more about how I built this blog check out <a href="https://medium.com/@justgnnr/how-to-build-a-static-blog-with-automated-a-i-generated-content-fc7f05b6b084">my Medium article</a> about the project.
           </p>
         </div>
       </div>
